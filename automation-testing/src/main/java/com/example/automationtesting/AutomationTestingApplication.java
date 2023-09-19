6c23c8cb4a00419f2c2f5c7c3a31702a947616e6
Usman Naveed <usmna.naveed@2iqresearch.com>
2023-09-19 16:13:09 +0500

    adding selenium maven dependencies

---
+++ automation-testing/src/main/java/com/example/automationtesting/AutomationTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.automationtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomationTestingApplication.class, args);
+	}
+
+}
